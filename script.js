20d7fa7eb55f182207955570f1b7e94b948a3bad
ragedragon98 <70817423+ragedragon98@users.noreply.github.com>
2026-02-14 04:06:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -54,12 +54,12 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
+    "betul bah?",
+    "Yakinnn??",
+    "uih,jangan bhh",
     "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
+    "cuba pikir bagus-bagus!",
+    "Sedih lah aku ni tau",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
